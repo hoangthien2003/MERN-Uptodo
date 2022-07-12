00717f22c76d7f0c98065ea01800b1218d45be0e
Hoang Thien <hoangdangbaothien@gmail.com>
2022-07-12 16:15:28 +0700

    v1.0.4 Secure login

--- client/src/screens/TodoScreen.jsx
+++ client/src/screens/TodoScreen.jsx
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import "../styles/TodoScreen.css";
 import "../components/TodoScreen/CompletedTasks";
 import CompletedTasks from "../components/TodoScreen/CompletedTasks";
 import ImportantTasks from "../components/TodoScreen/ImportantTasks";
 import TodoTasks from "../components/TodoScreen/TodoTasks";
+import { useSelector } from "react-redux";
+import { loginSelector } from "../redux/selectors";
+import { useNavigate } from "react-router-dom";
 
 export default function TodoScreen() {
+  const isAuth = useSelector(loginSelector);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!isAuth) navigate("../login", { replace: true });
+  });
+
   return (
     <div className="container">
       <CompletedTasks />
